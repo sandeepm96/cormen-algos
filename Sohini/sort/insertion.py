9b3a668cc839308effce71cdbb09db494f95f3df
sohini-roy <sohinirc45@gmail.com>
2017-09-13 16:14:42 +0530

    insertion sort

--- Sohini/sort/insertion.py
+++ Sohini/sort/insertion.py
@@ -1,38 +1,17 @@
-def insertion_sort(l):
-    for i in range(1, len(l)):
-        j = i-1
-        key = l[i]
-        while (j >= 0) and (l[j] > key):
-           l[j+1] = l[j]
-           j -= 1
-        l[j+1] = key
+class Insertionsort:
+    def __init__ (self, input_array):
+        self.array = input_array
 
-m = int(input().strip())
-ar = [int(i) for i in input().strip().split()]
-insertion_sort(ar)
-print(" ".join(map(str,ar)))
+    def insertion_sort(self):
+        array = self.array
+        for i in range(1, len(array)):
+            j = i-1
+            key = array[i]
+            while (j >= 0) and (array[j] > key):
+               array[j+1] = array[j]
+               j -= 1
+            array[j+1] = key
+        self.array = array
 
-n= int(input());
-arr = [];
-
-for i in range(0, n):
-    arr[i] = int(input());
-
-def findpos(i):
-    larger = 0;
-    for j in range(0,i):
-        if arr[j]>arr[i]:
-            larger +=1;
-    return larger;
-
-def move(i):
-    l = n;
-    while(l >= i):
-        arr[l+1] = arr[l];
-        l -= 1;
-
-for i in range(0, n):
-    x = arr[i];
-    y = i - findpos(i);
-    move(y);
-    arr[y] = x;
+    def result(self):
+        return self.array
