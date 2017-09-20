4a92e3134cbb37e426b582e868385757873972ee
sohini-roy <sohinirc45@gmail.com>
2017-09-20 17:25:03 +0530

    binary search tree

--- Sohini/heapsort.py
+++ Sohini/heapsort.py
@@ -1,7 +1,7 @@
 class Heapsort:
     def __init__ (self, input_array):
         self.array = input_array
-        
+
     def heap_sort(self):
         self.build_heap()
         for i in range(self.heapsize -1, -1, -1):
@@ -10,17 +10,16 @@ class Heapsort:
             self.max_heapify(0)
 
     def build_heap(self):
-        self.heapsize = len(self.array)
-        for i in range(len(self.array)/2, 0, -1):
+        self.heapsize = int(len(self.array))
+        for i in range(len(self.array)/2, -1, -1):
             self.max_heapify(i)
 
     def max_heapify(self, i):
+        largest = i
         l = 2*i + 1
         r = 2*i + 2
         if l < self.heapsize and self.array[l] > self.array[i]:
             largest = l
-        else:
-            largest = i
         if r < self.heapsize and self.array[r] > self.array[largest]:
             largest = r
         if largest != i:
@@ -30,3 +29,8 @@ class Heapsort:
     def result(self):
         self.heap_sort()
         return self.array
+
+array = [34, 23, 7, 1, 67, 3]
+a = Heapsort(array)
+array = a.result()
+print(array )
