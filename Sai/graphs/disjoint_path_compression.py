26d2b452f535e3a6f8ab6e99f356ee485554cd46
Saisankar Gochhayat <saisankargochhayat@gmail.com>
2017-10-01 12:57:28 +0530

    added dijkstra for adj matrix

--- Sai/graphs/disjoint_path_compression.py
+++ Sai/graphs/disjoint_path_compression.py
@@ -1,13 +1,12 @@
 from collections import defaultdict
 class Graph:
     def __init__(self,vertices):
+        self.graph = defaultdict(list)
         self.V = vertices
-        self.graph = []
 
-    def addEdge(self,u,v,w):
-        self.graph.append([u,v,w])
-    # A utility function to find set of an element i
-    # (uses path compression technique)
+    def addEdge(self,u,v):
+        self.graph[u].append(v)
+    
     def find(self,parent,i):
         if parent[i] == i:
             return i
@@ -28,35 +27,27 @@ class Graph:
         else :
             parent[yroot] = xroot
             rank[xroot] += 1
-    def KruskalMST(self):
-        result = []
-        i = 0 #index for sorted edges
-        e = 0 #index for result[]
-        #sorted by weight
-        self.graph = sorted(self.graph,key=lambda item:item[2])
-        # print(self.graph[0])
+    def isCyclic(self):
         parent = []
         rank = []
         for node in range(self.V):
             parent.append(node)
             rank.append(0)
-        while e < self.V -1:
-            u,v,w = self.graph[i]
-            i = i+1
-            x = self.find(parent,u)
-            y = self.find(parent,v)
-            if x!=y:
-                e= e+1
-                result.append([u,v,w])
+        for i in self.graph:
+            for j in self.graph[i]:
+                x = self.find(parent,i)
+                y = self.find(parent,j)
+                if x == y:
+                    return True
                 self.union(parent,rank,x,y)
-            #else discard edge
-        print(result)
+        return False
 
 g = Graph(4)
-g.addEdge(0, 1, 10)
-g.addEdge(0, 2, 6)
-g.addEdge(0, 3, 5)
-g.addEdge(1, 3, 15)
-g.addEdge(2, 3, 4)
- 
-g.KruskalMST()
+g.addEdge(0, 1)
+g.addEdge(1, 2)
+g.addEdge(2, 3)
+g.addEdge(3, 0)
+if g.isCyclic():
+    print("Graph contains cycle")
+else :
+    print("Graph does not contain cycle ")
